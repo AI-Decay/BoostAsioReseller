474535fcce6f03db873c371faf2a3df59a2ac72e
Remez Pavel <github.aidecay@gmail.com>
2021-03-31 21:26:51 +0300

    Init

---
+++ BoostAsioReseller/BoostAsioReseller.cpp
@@ -0,0 +1,12 @@
+﻿// BoostAsioReseller.cpp : Defines the entry point for the application.
+//
+
+#include "BoostAsioReseller.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
