e978784a908eddee2ea339e6ad2b2987a69f9731
Remez Pavel <github.aidecay@gmail.com>
2021-04-10 20:07:52 +0300

    Refactor project directory

---
+++ BoostAsioReseller/src/main/boost_asio_reseller.cpp
@@ -0,0 +1,5 @@
+﻿// BoostAsioReseller.cpp : Defines the entry point for the application.
+//
+
+#include "boost_asio_reseller.h"
+
---
+++ BoostAsioReseller/src/main/boost_asio_reseller.h
@@ -0,0 +1,9 @@
+﻿#ifndef BOOST_ASIO_RESELLER
+#define BOOST_ASIO_RESELLER
+#pragma once
+class boost_asio_reseller
+{
+	
+};
+
+#endif //BOOST_ASIO_RESELLER
