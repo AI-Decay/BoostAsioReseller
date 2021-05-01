0afee2ad1f536f15748c3f7f7b141f0759bd34f4
Remez Pavel <github.aidecay@gmail.com>
2021-05-01 23:57:11 +0300

    Change ip&port and add sleep before stop

--- BoostAsioReseller/src/main/main.cpp
+++ BoostAsioReseller/src/main/main.cpp
@@ -2,9 +2,10 @@
 #include <iostream>
 
 int main() {
-  tcp_socket::tcp_listener listener(boost::asio::ip::address_v4::any(), 100,
-                                    100000);
+  tcp_socket::tcp_listener listener(
+      boost::asio::ip::address_v4({192, 168, 31, 6}), 1, 64);
   listener.open();
+  std::this_thread::sleep_for(std::chrono::milliseconds(10000));
   listener.close();
 
   return 0;
